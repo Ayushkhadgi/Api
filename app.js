12b92ebeb0f022fadb9174b2322b8c3dc5badd5e
Ayush Ak <khadgiayush136@gmail.com>
2022-10-19 21:24:07 +0530

    api

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let mongoUrl = "mongodb+srv://ayush:ayush123@cluster0.fvalbpb.mongodb.net/Vision
 let bodyParser = require('body-parser');
 let db;
 
-    // middleware
+// middleware
 app.use(morgan('short',{stream:fs.createWriteStream('./app.logs')}))
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
@@ -24,7 +24,7 @@ app.get('/',(req,res) => {
     res.send('This is From Express App code')
 })
 
-    // List of Category
+// List of Category
 
 app.get('/category',(req,res)=>{
     db.collection('category').find().toArray((err,result)=>{
@@ -33,7 +33,7 @@ app.get('/category',(req,res)=>{
     })
 })
 
-    // List of Items
+// List of Items
 
 app.get('/items',(req,res)=>{
     db.collection('data').find().toArray((err,result)=>{
@@ -42,7 +42,7 @@ app.get('/items',(req,res)=>{
     })
 })
 
-    // List of Items wrt Category
+// List of Items wrt Category
 
 
  app.get('/products',(req,res) => {
@@ -57,7 +57,7 @@ app.get('/items',(req,res)=>{
     })
 })
 
-    // List of Items wrt Brand
+// List of Items wrt Brand
 
 
 app.get('/brand',(req,res) => {
@@ -72,7 +72,7 @@ app.get('/brand',(req,res) => {
     })
 })
 
-    // Items wrt cost & Brand & sort
+// Items wrt cost & Brand & sort
 
 
 app.get('/filter/:brandid',(req,res) => {
@@ -99,7 +99,7 @@ app.get('/filter/:brandid',(req,res) => {
     })
 })
 
-    // Details of the product
+// Details of the product
 
 
 app.get('/details/:id',(req,res) => {
@@ -111,7 +111,7 @@ app.get('/details/:id',(req,res) => {
         })
     })
 
-    // Item Details
+ // Item Details
 
 app.post('/Itemdetails',(req,res) => {
     if(Array.isArray(req.body.id)){
@@ -124,7 +124,7 @@ app.post('/Itemdetails',(req,res) => {
     }
 })
 
-    // List of orders
+ // List of orders
 
 
  app.get('/orders',(req,res) => {
@@ -140,7 +140,7 @@ app.post('/Itemdetails',(req,res) => {
         })
     })
 
-    // Place Order
+ // Place Order
 
 
  app.post('/placeOrder',(req,res) => {
@@ -180,10 +180,147 @@ app.post('/Itemdetails',(req,res) => {
                     res.send('Order Deleted')
                 })
             })
+            
+    
+// // list of restaurant
+// app.get('/restaurants',(req,res) => {
+//     let query = {};
+//     let stateId = Number(req.query.stateId);
+//     let mealId = Number(req.query.mealId);
+//     if(stateId){
+//         query={state_id:stateId}
+//     }else if(mealId){
+//         query={"mealTypes.mealtype_id":mealId}
+//     }
+//     db.collection('restaurants').find(query).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
 
-    // connection with mongo
+// app.get('/filter/:mealId',(req,res) => {
+//     let query = {};
+//     let sort = {cost:1}
+//     let mealId = Number(req.params.mealId);
+//     let cuisineId = Number(req.query.cuisineId);
+//     let lcost = Number(req.query.lcost);
+//     let hcost = Number(req.query.hcost);
+//     if(req.query.sort){
+//         sort={cost:req.query.sort}
+//     }
+//     if(cuisineId && lcost && hcost){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             "cuisines.cuisine_id":cuisineId,
+//             $and:[{cost:{$gt:lcost,$lt:hcost}}]
+//         }
+//     }
+//     else if(cuisineId){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             "cuisines.cuisine_id":cuisineId
+//         }
+//     }else if(lcost && hcost){
+//         query={
+//             "mealTypes.mealtype_id":mealId,
+//             $and:[{cost:{$gt:lcost,$lt:hcost}}]
+//         } 
+//     }
+//     db.collection('restaurants').find(query).sort(sort).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
+
+// // list of meals
+// app.get('/meals',(req,res) => {
+//     db.collection('mealType').find().toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
+
+// //restaurant details
+// app.get('/details/:id',(req,res) => {
+//     //let id = mongo.ObjectId(req.params.id)
+//     let id = Number(req.params.id)
+//     db.collection('restaurants').find({restaurant_id:id}).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
+
+// restaurant menu details
+// app.get('/menu/:id',(req,res) => {
+//     let id = Number(req.params.id)
+//     db.collection('menu').find({restaurant_id:id}).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
+
+// app.post('/placeOrder',(req,res) => {
+//     console.log(req.body);
+//     db.collection('orders').insert(req.body,(err,result) => {
+//         if(err) throw err;
+//         res.send('Order Placed')
+//     })
+// })
+
+// // //menu details
+// app.post('/menuItem',(req,res) => {
+//     if(Array.isArray(req.body.id)){
+//         db.collection('menu').find({menu_id:{$in:req.body.id}}).toArray((err,result) =>{
+//             if(err) throw err;
+//             res.send(result)
+//         })
+//     }else{
+//         res.send('Inavlid Input')
+//     }
+// })
 
 
+// //list of order
+// app.get('/orders',(req,res) => {
+//     let email = req.query.email
+//     let query = {};
+//     if(email){
+//        // query={email:email}
+//         query={email}
+//     }
+//     db.collection('orders').find(query).toArray((err,result) =>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+// })
+
+// app.put('/updateOrder/:id',(req,res) => {
+//     let oid = Number(req.params.id);
+//     db.collection('orders').updateOne(
+//         {orderId:oid},
+//         {
+//             $set:{
+//                 "status":req.body.status,
+//                 "bank_name":req.body.bank_name,
+//                 "date":req.body.date
+//             }
+//         },(err,result) => {
+//             if(err) throw err;
+//             res.send('Order Updated')
+//         }
+//     )
+// })
+
+// app.delete('/deleteOrder/:id',(req,res) => {
+//     let _id = mongo.ObjectId(req.params.id);
+//     db.collection('orders').deleteOne({_id},(err,result) => {
+//         if(err) throw err;
+//         res.send('Order Deleted')
+//     })
+// })
+
+
+// connection with mongo
 MongoClient.connect(mongoUrl,(err,client)=>{
     if(err) console.log(`Error while connecting`);
     db = client.db('Visionrays')
